dfc59c108e342b5ad0186ef90f84b7403e1730fe
Gavin Hill <142995904+gHill27@users.noreply.github.com>
2026-01-25 17:18:48 -0500

    added lab 1 files

---
+++ Turtles_Code.py
@@ -0,0 +1,16 @@
+import turtle
+
+t = turtle.Turtle()
+t.left(90)
+for i in range(3):
+    for i in range(3):
+        t.forward(100)   # length of each side
+        t.right(120)      # turn angle for a triangle
+    t.left(120)
+
+t.forward(100)
+t.left(90)
+t.circle(100)
+
+turtle.done()
+
